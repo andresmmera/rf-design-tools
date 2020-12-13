1d8529d1c2c03bb62bbc46ff154f0e682a2d973e
andresmmera <andresmartinezmera@gmail.com>
2020-12-13 18:33:58 +0100

    RF Power converter tool

--- rf_design_tools/catalog/forms.py
+++ rf_design_tools/catalog/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-# FORM 1 - Calculation of the load impedance given the reflection coefficient
+# FORM 1.1 - Calculation of the load impedance given the reflection coefficient
 
 class ReflectionCoefficientToImpedanceForm(forms.Form):
     
@@ -16,14 +16,31 @@ class ReflectionCoefficientToImpedanceForm(forms.Form):
         self.fields['gamma_mag'].widget.attrs['max'] = 1
 
 
-# FORM 2 - Calculation of the reflection coefficient given the load impedance
+# FORM 1.2 - Calculation of the reflection coefficient given the load impedance
 class ImpedanceToReflectionCoefficientForm(forms.Form):
     
-    ZR = forms.FloatField(initial=30,  label='Re{Z}')
-    ZI = forms.FloatField(initial=15, label='Im{Z}')   
+    ZR = forms.FloatField(initial=73.44,  label='Re{Z}')
+    ZI = forms.FloatField(initial=7.92, label='Im{Z}')   
     Z0 = forms.FloatField(initial=50, label='Z\u2080')
 
     def __init__(self, *args, **kwargs):
         super(ImpedanceToReflectionCoefficientForm, self).__init__(*args, **kwargs)
         self.fields['Z0'].widget.attrs['min'] = 1e-6
         self.fields['ZR'].widget.attrs['min'] = 1e-6
+
+# FORM 2 - RF POWER CONVERTER
+class RF_PowerConversionForm(forms.Form):
+    
+    P = forms.FloatField(initial=0,  label='Power')
+    CHOICES_Units = (
+        ('0', "W"),
+        ('1', "dBm"),
+        ('2', "dB\u00B5V (Z\u2080 = 75 \u03A9)"),
+        ('3', "dBmV (Z\u2080 = 75 \u03A9)"),
+        ('4', "dB\u00B5V (Z\u2080 = 50 \u03A9)"),
+        ('5', "dBmV (Z\u2080 = 50 \u03A9)"),
+        ('6', "dBpW"),
+    )
+    old_units = forms.ChoiceField(choices=CHOICES_Units, label='Units', initial='1')
+    new_units = forms.ChoiceField(choices=CHOICES_Units, label='New units', initial='2')
+
--- rf_design_tools/catalog/urls.py
+++ rf_design_tools/catalog/urls.py
@@ -3,5 +3,6 @@ from catalog import views
 
 urlpatterns = [
     path('', views.ToolListView.as_view(), name='tools'),
-    path('tool/reflection_coefficient', views.CalculateReflectionCoefficientForm, name='reflection-coeff'),
+    path('tool/reflection_coefficient', views.CalculateReflectionCoefficientView, name='reflection-coeff'),
+    path('tool/rf_power_converter', views.CalculatePowerConverterView, name='rf_power_converter'),
 ]
